36f42f9075733f6065e25c106e8d07fa164bb562
William Harrington <kb0iic@berzerkula.org>
2021-12-16 13:43:04 -0600

    Adjust client API version to at least 3.3.

--- Tutorial/init.cpp
+++ Tutorial/init.cpp
@@ -10,8 +10,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 GLFWwindow* initWindow(int width, int height)
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); OS X
 
