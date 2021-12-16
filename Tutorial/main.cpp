2a715fc6443f1cb97be83d9d4659f502a4b15b15
William Harrington <kb0iic@berzerkula.org>
2021-12-16 14:01:41 -0600

    Draw two triangles for a rectangle.

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -9,11 +9,11 @@ int success;
 char infoLog[512];
 
 const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
@@ -71,20 +71,30 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left
-         0.5f, -0.5f, 0.0f, // right
-         0.0f,  0.5f, 0.0f  // top
+         0.5f,  0.5f, 0.0f, // top right
+		 0.5f, -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f  // top left
 	};
 
-	// Vertex Buffer Object and Vertex Array Object
-	unsigned int VAO, VBO;
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    // Vertex Buffer Object, Vertex Array Object and Element Buffer Object
+	unsigned int EBO, VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Set the vertex attribute pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -94,6 +104,9 @@ int main()
 
 	glBindVertexArray(0);
 
+    // Uncomment for wireframe polygons
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while(!glfwWindowShouldClose(window))
     {
         // input
@@ -101,11 +114,18 @@ int main()
 
         // rendering
         clearWithColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw the first triangle
+        // draw the first triangle or second triangle for the rectangle
         glUseProgram(shaderProgram);
+        
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Triangle
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Rectangle
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
